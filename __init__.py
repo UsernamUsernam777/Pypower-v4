323c94d75c8f8f79e3a6a1f2c2a406d3b53a0487
Mohamed / モハメド <me4564104@gmail.com>
2026-04-06 00:09:32 +0200

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from . import Files
-from . import GUI
-from . import Iterable
-from . import Math
-from . import Other
-from . import String
-from . import Time
-
-__all__ = ['Time', 'Other', 'Files', 'GUI', 'String', 'Iterable', 'Math']
